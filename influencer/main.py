2d667949a8ebf3ef099cec9ab96d975d2f54d3d2
bmangir <berkantmangir23@gmail.com>
2024-08-08 17:36:40 +0300

    simplify the finding ranking

--- influencer/main.py
+++ influencer/main.py
@@ -17,12 +17,14 @@ def read_youtuber_details(path) -> DataFrame:
         .withColumn("video_views", regexp_replace("video_views", ',', '').cast(LongType())) \
         .withColumn("video_count", regexp_replace("video_count", ',', '').cast(LongType()))
 
+    df = find_coefficient_factor(df)
+
     return df
 
 
 def find_coefficient_factor(df) -> DataFrame:
     """
-    coefficient = total video views / video count * 10^9
+    coefficient = total video views / video count * 10^(-11)
     Find the coefficient where is not Null
     :param df: dataframe
     :return: updated dataframe with coefficient column
@@ -31,21 +33,34 @@ def find_coefficient_factor(df) -> DataFrame:
     df = df \
         .withColumn("video_views", coalesce(col("video_views"), lit(0))) \
         .withColumn("video_count", coalesce(col("video_count"), lit(0))) \
-        .withColumn("coefficient", round(col("video_count") / col("video_views") * pow(10, 9), 4)) \
+        .withColumn("coefficient", round(col("video_views") / col("video_count") * col("subscribers") * pow(10, -11), 4)) \
         .where(col("coefficient").isNotNull())
-
+# TODO: kubernetes
     return df
 
 
-def rank_by_category_n_subscribers(df) -> DataFrame:
+def rank_by_coefficient(df) -> DataFrame:
     """
 
     :param df:
     :return:
     """
 
-    category_window = Window.partitionBy("category").orderBy(col("coefficient").asc())
-    df = df.withColumn('rnk', rank().over(category_window))
+    category_window = Window.partitionBy("category").orderBy(col("coefficient").desc())
+    df = df.withColumn('cf_rnk', dense_rank().over(category_window))
+
+    return df
+
+
+def rank_by_subscribers(df) -> DataFrame:
+    """
+
+    :param df:
+    :return:
+    """
+
+    category_window = Window.partitionBy("category").orderBy(col("subscribers").asc())
+    df = df.withColumn('subs_rnk', dense_rank().over(category_window))
 
     return df
 
@@ -59,14 +74,14 @@ def get_top_n_all_channels(df, n) -> DataFrame:
     :return: dataframe with top N channels for each category
     """
 
-    return df.where(col("rnk") <= n)
+    return df.where(col("cf_rnk") <= n)
 
 
-def get_top_n_channels_by_category(df, category, n) -> DataFrame:
+def get_top_n_channels_by_category(df, n, category=None) -> DataFrame:
     if category is None:
         return df.where(col("category").isNull()).limit(n)
 
-    return df.where(col("category") == category).limit(n)
+    return get_top_n_all_channels(df.where(col("category") == category.capitalize()), n)
 
 
 # Create spark session
@@ -75,18 +90,15 @@ youtube_session = create_spark_session("Youtube Analysis")
 # Read the input data
 input_path = "data/most_subscribed_youtube_channels.csv"
 ranked_youtubers = read_youtuber_details(input_path)
-ranked_youtubers = find_coefficient_factor(ranked_youtubers)
 
 # Order by coefficient values for each category
-ranked_by_category = rank_by_category_n_subscribers(ranked_youtubers)
+ranked_by_category = rank_by_coefficient(ranked_youtubers)
+ranked_by_category = rank_by_subscribers(ranked_by_category)
 
-# Find the distinct categories
-categories = ranked_youtubers.select("category").distinct().collect()
-
-# Find the top 5 channels for each category
-top_channels_for_each_category = get_top_n_all_channels(ranked_by_category, 5)
+# Find the top n channels for each category
+top_channels_for_each_category = get_top_n_all_channels(ranked_by_category, n=1)
 top_channels_for_each_category.show()
 
 # Find the top 10 channels in Music category
-top_music_channels = get_top_n_channels_by_category(ranked_by_category, "Music", 10)
+top_music_channels = get_top_n_channels_by_category(ranked_by_category, 10, "Sports").orderBy("cf_rnk")
 top_music_channels.show()
